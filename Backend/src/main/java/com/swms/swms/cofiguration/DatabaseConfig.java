b325abd0a0fb9139397bb4cecda4bce4cc3eabbf
shivashankarmn <131743146+shivashankarmn@users.noreply.github.com>
2025-04-16 09:51:01 +0530

    Update DatabaseConfig.java

--- Backend/src/main/java/com/swms/swms/cofiguration/DatabaseConfig.java
+++ Backend/src/main/java/com/swms/swms/cofiguration/DatabaseConfig.java
@@ -1,30 +1,30 @@
-package com.swms.swms.cofiguration;
+// package com.swms.swms.cofiguration;
  
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
  
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.jdbc.datasource.DriverManagerDataSource;
  
-import com.swms.swms.util.SecretsManagerUtil;
+// import com.swms.swms.util.SecretsManagerUtil;
  
-@Configuration
-public class DatabaseConfig {
+// @Configuration
+// public class DatabaseConfig {
  
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-    @Value("${spring.datasource.username}")
-    private String dbUsername;
+//     @Value("${spring.datasource.url}")
+//     private String dbUrl;
+//     @Value("${spring.datasource.username}")
+//     private String dbUsername;
  
-    @Bean
-    public DataSource dataSource() {
-        String dbPassword = SecretsManagerUtil.getRDSPassword();
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl(dbUrl);
-        dataSource.setUsername(dbUsername);
-        dataSource.setPassword(dbPassword);
-        return dataSource;
-    }
-}
+//     @Bean
+//     public DataSource dataSource() {
+//         String dbPassword = SecretsManagerUtil.getRDSPassword();
+//         DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//         dataSource.setUrl(dbUrl);
+//         dataSource.setUsername(dbUsername);
+//         dataSource.setPassword(dbPassword);
+//         return dataSource;
+//     }
+// }
